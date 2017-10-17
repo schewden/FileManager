dae4fd3de81c49cb4fa675aa6b20a1c0fb8a72d0
Shevelev Denis <shevdionis@gmail.com>
2017-10-17 21:27:42 +0300

    Proceeded to perform the test task.

---
+++ src/main/java/com/shevelev/manager/HeadFileManager.java
@@ -0,0 +1,11 @@
+package com.shevelev.manager;
+
+/**
+ * Created by denis on 17.10.17.
+ */
+
+public class HeadFileManager {
+    public static void main(String[] args) {
+        System.out.println("Started work! Date: 17.10.2017 ");
+    }
+}
